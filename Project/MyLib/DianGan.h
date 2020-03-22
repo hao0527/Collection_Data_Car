34f80725ecdb60cba5b24427c051d663576d0e08
hao0527 <33110820@qq.com>
2020-03-22 10:35:15 +0800

    Signed-off-by: hao0527 <33110820@qq.com>

--- Project/MyLib/DianGan.h
+++ Project/MyLib/DianGan.h
@@ -2,10 +2,10 @@
 #define __DianGan_H__
 
 
-extern float XL, YL, XM, YR, XR;      													   	//全局变量
-extern float X1, X2, X3, X4, X5, X6, X7;													//全局变量
-extern float diffX, diffY;																	//全局变量
-extern float NormalizationX, NormalizationY;												//全局变量
+extern float XL, YL, XM, YR, XR;                //全局变量
+extern float X1, X2, X3, X4, X5, X6, X7;        //全局变量
+extern float diffX, diffY;                      //全局变量
+extern float NormalizationX, NormalizationY;    //全局变量
 
 
 void DG_ADC_Init();
--- Project/USER/src/main.c
+++ Project/USER/src/main.c
@@ -15,14 +15,14 @@ uint16 adc_value;
 void main(void)
 {
     DisableGlobalIRQ();
-    board_init();					//务必保留，本函数用于初始化MPU 时钟 调试串口
-	DG_ADC_Init();					//电感ADC初始化
+    board_init();                   //务必保留，本函数用于初始化MPU 时钟 调试串口
+    DG_ADC_Init();                  //电感ADC初始化
 
-	//此处编写用户代码(例如：外设初始化代码等)
+    //此处编写用户代码(例如：外设初始化代码等)
 
-    EnableGlobalIRQ(0);				//总中断最后开启
+    EnableGlobalIRQ(0);             //总中断最后开启
     while(1)
     {
-		//此处编写需要循环执行的代码
+        //此处编写需要循环执行的代码
     }
 }
